973b4384a0676b171244f8798d7ffcfbbd997896
vanishree00 <vanishreemalli2007@gmail.com>
2026-08-19 11:28:16 +0530

    default value  for prop

--- Day/Day 13/day-13-react-app/src/App.jsx
+++ Day/Day 13/day-13-react-app/src/App.jsx
@@ -1,10 +1,22 @@
 import { Welcome } from "./components/P1";
+import { ProductCard} from "./components/P2";
+import  SonyImg  from "./assets/sony.jpg";
+
+import './App.css'
+
 function App() {
   
 
   return (
     <>
       <Welcome name="vanishree" />
+      <ProductCard
+      name="sony"
+      price={4900}
+      image={SonyImg}
+      
+      
+      />
     </>
   )
 }
